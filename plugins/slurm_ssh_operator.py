495b20ea2d0f3e4917967bfc67bd54f2288417e1
longle-intersect <long@intersect.org.au>
2024-10-09 14:48:28 +0000

    Update SSHOperator

--- plugins/slurm_ssh_operator.py
+++ plugins/slurm_ssh_operator.py
@@ -80,16 +80,20 @@ class SlurmJobSensor(BaseSensorOperator):
                 else:
                     # Fetch the output and error files
                     sftp_client = ssh_client.open_sftp()
-                    output_file = 'test_job_error.out'
-                    error_file = 'test_job_output.txt'
-                    sftp_client.get(self.remote_path, output_file)
-                    sftp_client.get(self.remote_path, error_file)
+                    output_file = f'{self.remote_path}/test_job_error.out'
+                    error_file = f'{self.remote_path}/test_job_output.txt'
+                    local_output_path = '/home/airflow/slurm_scripts/test_job_error.out'
+                    local_error_path = '/home/airflow/slurm_scripts/test_job_output.txt'
+                    sftp_client.get(output_file, local_output_path)
+                    sftp_client.get(error_file, local_error_path)
+                    output_content = sftp_client.open(output_file).read().decode('utf-8')
+                    error_content = sftp_client.open(error_file).read().decode('utf-8')
                     sftp_client.close()
 
                     self.log.info(f"Output and error files retrieved: {output_file}, {error_file}")
                     self.log.info(f"Job {job_id} has completed or does not exist")
                     
-                    return output_file
+                    return output_content
                 
         except IOError as e:
             self.log.error(f"Error fetching files: {str(e)}")
