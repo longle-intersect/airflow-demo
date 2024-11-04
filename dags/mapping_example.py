f2b1e2d1ee6f03827aab6219ed96eba786572e60
longle-intersect <long@intersect.org.au>
2024-11-04 00:37:56 +0000

    Upload sample dynamic task

---
+++ dags/mapping_example.py
@@ -0,0 +1,20 @@
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+import random
+
+
+@dag(dag_id="test_dynamic_mapping", start_date=days_ago(2), schedule=None, catchup=False)
+def mapping_example():
+
+    @task
+    def make_list():
+        return [i + 1 for i in range(random.randint(2, 4))]
+
+    @task
+    def consumer(value):
+        print(repr(value))
+
+    consumer.expand(value=make_list())
+
+
+mapping_example()
