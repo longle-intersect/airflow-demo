a9826ca7e9365508a38f454caf4c31e158b20269
longle-intersect <long@intersect.org.au>
2024-11-07 06:46:54 +0000

    Update sentinel batch

--- dags/test_dag_loop_dtm.py
+++ dags/test_dag_loop_dtm.py
@@ -1,40 +1,40 @@
 import logging
 import sys
+import os
 from datetime import datetime
-from airflow.decorators import dag, task
+from airflow.decorators import dag
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-# Function to return the current time
 def return_time():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Function to calculate time elapsed
 def calculate_time_elapsed(**kwargs):
-    ti = kwargs.get('ti')
+
+    ti  = kwargs.get('ti')
     xcom_val = ti.xcom_pull(task_ids="start_timer_task")
     start_time = datetime.strptime(xcom_val, '%Y-%m-%d %H:%M:%S')
     end_time = datetime.strptime(return_time(), '%Y-%m-%d %H:%M:%S')
+
     return end_time - start_time
 
-# Function to return a list of numbers for dynamic task mapping
 def one_two_three_traditional():
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    # This is the array that is used to meausre the run times between the 2 approaches
+    return [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15]]
 
-# Function to add 10 to a given number
 def plus_10_traditional(x):
     return x + 10
 
-# Function to subtract 3 from a given number
 def minus_3_traditional(x):
     return x - 3
 
 @dag(
     dag_id="test_dag_loop_dmt",
     start_date=datetime(2024, 4, 1),
-    schedule_interval='@daily',
+    schedule='@daily',
     catchup=False,
     default_args={
         "retries": 2,
@@ -57,13 +57,10 @@ def dynamic_tsk_map_dag():
         python_callable=one_two_three_traditional
     )
 
-    # Mapping the `plus_10_traditional` function dynamically
     plus_10_task = PythonOperator.partial(
-        task_id="plus_10_task",
-        python_callable=plus_10_traditional
+        task_id="plus_10_task", python_callable=plus_10_traditional
     ).expand(op_args=one_two_three_task.output)
 
-    # Mapping the `minus_3_traditional` function dynamically
     minus_3_task = PythonOperator.partial(
         task_id="minus_3_task", 
         python_callable=minus_3_traditional
@@ -78,7 +75,6 @@ def dynamic_tsk_map_dag():
         task_id='end_task'
     )
 
-    # Setting the dependencies
     start_task >>\
         start_timer_task >>\
         one_two_three_task >>\
@@ -87,4 +83,5 @@ def dynamic_tsk_map_dag():
         end_timer_task >>\
         end_task
 
-dynamic_dag = dynamic_tsk_map_dag()
+
+dynamic_tsk_map_dag()
