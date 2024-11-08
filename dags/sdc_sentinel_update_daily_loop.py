b7b147c6c48752d650ef532deffb4948c8c78632
longle-intersect <long@intersect.org.au>
2024-11-08 02:55:34 +0000

    Upload trigger DAG

--- dags/sdc_sentinel_update_daily_loop.py
+++ dags/sdc_sentinel_update_daily_loop.py
@@ -42,88 +42,84 @@ def daily_sentinel_batch_processing_dag():
                               do_xcom_push=True)
 
     # Combine all commands into one large script
-    # Task 1: Cloud fmask processing
-    cloud_fmask_processing = SlurmJobHandlingSensor.partial(
-        task_id='sentt_batch_s1',
-        ssh_conn_id='slurm_ssh_connection',
-        script_name='sentt_batch_s1',
-        remote_path=remote_path,
-        local_path=local_path, 
-        #stage_script=script_stage_1,
-        #dag=dag,
-        timeout=3600,
-        poke_interval=30,
-        #date = date,
-        stage = "1",
-        map_index_template="{{task.date}}"
-    ).expand(date=get_list.output)
+    for i, date in enumerate(get_dates()):
+        # Task 1: Cloud fmask processing
+        cloud_fmask_processing = SlurmJobHandlingSensor(
+            task_id=f'sentt_{date}_s1',
+            ssh_conn_id='slurm_ssh_connection',
+            script_name=f'sentt_{date}_s1',
+            remote_path=remote_path,
+            local_path=local_path, 
+            #stage_script=script_stage_1,
+            #dag=dag,
+            timeout=3600,
+            poke_interval=30,
+            date = date,
+            stage = "1"
+        )
 
-    # Task 2: Topo masks processing
-    topo_masks_processing = SlurmJobHandlingSensor.partial(
-        task_id='sentt_batch_s2',
-        ssh_conn_id='slurm_ssh_connection',
-        script_name='sentt_batch_s2',
-        remote_path=remote_path,
-        local_path=local_path, 
-        #stage_script=script_stage_1,
-        #dag=dag,
-        timeout=3600,
-        poke_interval=30,
-        #date = date,
-        stage = "2",       
-        map_index_template="{{task.date}}"
-    ).expand(date=get_list.output)
+        # Task 2: Topo masks processing
+        topo_masks_processing = SlurmJobHandlingSensor(
+            task_id=f'sentt_{date}_s2',
+            ssh_conn_id='slurm_ssh_connection',
+            script_name=f'sentt_{date}_s2',
+            remote_path=remote_path,
+            local_path=local_path, 
+            #stage_script=script_stage_1,
+            #dag=dag,
+            timeout=3600,
+            poke_interval=30,
+            date = date,
+            stage = "2",       
+        ).expand(date=get_list.output)
 
 
-    # Task 3: Surface reflectance processing
-    surface_reflectance_processing = SlurmJobHandlingSensor.partial(
-        task_id='sentt_batch_s3',
-        ssh_conn_id='slurm_ssh_connection',
-        script_name='sentt_batch_s3',
-        remote_path=remote_path,
-        local_path=local_path, 
-        #stage_script=script_stage_1,
-        #dag=dag,
-        timeout=3600,
-        poke_interval=30,
-        #date = date,
-        stage = "3",      
-        map_index_template="{{task.date}}"
-    ).expand(date=get_list.output)
+        # Task 3: Surface reflectance processing
+        surface_reflectance_processing = SlurmJobHandlingSensor(
+            task_id=f'sentt_{date}_s3',
+            ssh_conn_id='slurm_ssh_connection',
+            script_name=f'sentt_{date}_s3',
+            remote_path=remote_path,
+            local_path=local_path, 
+            #stage_script=script_stage_1,
+            #dag=dag,
+            timeout=3600,
+            poke_interval=30,
+            date = date,
+            stage = "3",      
+        )
 
-    # Task 4: Water index processing
-    water_index_processing = SlurmJobHandlingSensor.partial(
-        task_id='sentt_batch_s4',
-        ssh_conn_id='slurm_ssh_connection',
-        script_name='sentt_batch_s4',
-        remote_path=remote_path,
-        local_path=local_path, 
-        #stage_script=script_stage_1,
-        #dag=dag,
-        timeout=3600,
-        poke_interval=30,
-        #date = date,
-        stage = "4",      
-        map_index_template="{{task.date}}"
-    ).expand(date=get_list.output)
+        # Task 4: Water index processing
+        water_index_processing = SlurmJobHandlingSensor(
+            task_id=f'sentt_{date}_s4',
+            ssh_conn_id='slurm_ssh_connection',
+            script_name=f'sentt_{date}_s4',
+            remote_path=remote_path,
+            local_path=local_path, 
+            #stage_script=script_stage_1,
+            #dag=dag,
+            timeout=3600,
+            poke_interval=30,
+            date = date,
+            stage = "4",      
+        )
 
-    # Task 5: Fractional cover processing
-    fractional_cover_processing = SlurmJobHandlingSensor.partial(
-        task_id='sentt_batch_s5',
-        ssh_conn_id='slurm_ssh_connection',
-        script_name='sentt_batch_s5',
-        remote_path=remote_path,
-        local_path=local_path, 
-        #stage_script=script_stage_1,
-        #dag=dag,
-        timeout=3600,
-        poke_interval=30,
-        #date = date,
-        stage = "5",      
-        map_index_template="{{task.date}}"
-    ).expand(date=get_list.output)
+        # Task 5: Fractional cover processing
+        fractional_cover_processing = SlurmJobHandlingSensor(
+            task_id=f'sentt_{date}_s5',
+            ssh_conn_id='slurm_ssh_connection',
+            script_name=f'sentt_{date}_s5',
+            remote_path=remote_path,
+            local_path=local_path, 
+            #stage_script=script_stage_1,
+            #dag=dag,
+            timeout=3600,
+            poke_interval=30,
+            date = date,
+            stage = "5",      
+        )
 
-    # Task Dependency Setup
-    get_list >> cloud_fmask_processing >> topo_masks_processing >> surface_reflectance_processing >> water_index_processing >> fractional_cover_processing
+        # Task Dependency Setup
+        get_list >> cloud_fmask_processing >> topo_masks_processing >> surface_reflectance_processing >> water_index_processing >> fractional_cover_processing
 
 dag_instance = daily_sentinel_batch_processing_dag()
