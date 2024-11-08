c72f62dd8bef9841c74dc1bd6d20445bea773438
longle-intersect <long@intersect.org.au>
2024-11-08 02:51:00 +0000

    Upload trigger DAG

---
+++ dags/sdc_sentinel_update_daily_loop.py
@@ -0,0 +1,129 @@
+import sys
+import logging
+sys.path.insert(0, '/opt/airflow/dags/repo/plugins')
+from airflow import DAG
+from airflow.decorators import dag, task
+from airflow.operators.bash import BashOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+from airflow.operators.python import PythonOperator
+from slurm_job_handler_new import SlurmJobHandlingSensor
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+remote_path='/home/lelong/log_airflow_slurm/scripts/'
+local_path='/home/airflow/slurm_scripts/' 
+
+# DAG Configuration
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=2),
+    'start_date': datetime(2024, 11, 1),
+}
+
+
+@dag(dag_id='sdc_sentinel_batch_update_daily_loop',
+     default_args=default_args,
+     description='Daily Update Sentinel-2 Imagery Loop on SDC',
+     schedule_interval=None,
+     start_date=days_ago(1),
+     tags=['sdc', 'sentinel'])
+def daily_sentinel_batch_processing_dag():
+
+    def get_dates():
+        return ["20241018", "20241011", "20241008", "20241001"]  # Assuming these dates are dynamically determined elsewhere
+
+    get_list = PythonOperator(task_id="get_img_list",
+                              python_callable=get_dates,
+                              do_xcom_push=True)
+
+    # Combine all commands into one large script
+    # Task 1: Cloud fmask processing
+    cloud_fmask_processing = SlurmJobHandlingSensor.partial(
+        task_id='sentt_batch_s1',
+        ssh_conn_id='slurm_ssh_connection',
+        script_name='sentt_batch_s1',
+        remote_path=remote_path,
+        local_path=local_path, 
+        #stage_script=script_stage_1,
+        #dag=dag,
+        timeout=3600,
+        poke_interval=30,
+        #date = date,
+        stage = "1",
+        map_index_template="{{task.date}}"
+    ).expand(date=get_list.output)
+
+    # Task 2: Topo masks processing
+    topo_masks_processing = SlurmJobHandlingSensor.partial(
+        task_id='sentt_batch_s2',
+        ssh_conn_id='slurm_ssh_connection',
+        script_name='sentt_batch_s2',
+        remote_path=remote_path,
+        local_path=local_path, 
+        #stage_script=script_stage_1,
+        #dag=dag,
+        timeout=3600,
+        poke_interval=30,
+        #date = date,
+        stage = "2",       
+        map_index_template="{{task.date}}"
+    ).expand(date=get_list.output)
+
+
+    # Task 3: Surface reflectance processing
+    surface_reflectance_processing = SlurmJobHandlingSensor.partial(
+        task_id='sentt_batch_s3',
+        ssh_conn_id='slurm_ssh_connection',
+        script_name='sentt_batch_s3',
+        remote_path=remote_path,
+        local_path=local_path, 
+        #stage_script=script_stage_1,
+        #dag=dag,
+        timeout=3600,
+        poke_interval=30,
+        #date = date,
+        stage = "3",      
+        map_index_template="{{task.date}}"
+    ).expand(date=get_list.output)
+
+    # Task 4: Water index processing
+    water_index_processing = SlurmJobHandlingSensor.partial(
+        task_id='sentt_batch_s4',
+        ssh_conn_id='slurm_ssh_connection',
+        script_name='sentt_batch_s4',
+        remote_path=remote_path,
+        local_path=local_path, 
+        #stage_script=script_stage_1,
+        #dag=dag,
+        timeout=3600,
+        poke_interval=30,
+        #date = date,
+        stage = "4",      
+        map_index_template="{{task.date}}"
+    ).expand(date=get_list.output)
+
+    # Task 5: Fractional cover processing
+    fractional_cover_processing = SlurmJobHandlingSensor.partial(
+        task_id='sentt_batch_s5',
+        ssh_conn_id='slurm_ssh_connection',
+        script_name='sentt_batch_s5',
+        remote_path=remote_path,
+        local_path=local_path, 
+        #stage_script=script_stage_1,
+        #dag=dag,
+        timeout=3600,
+        poke_interval=30,
+        #date = date,
+        stage = "5",      
+        map_index_template="{{task.date}}"
+    ).expand(date=get_list.output)
+
+    # Task Dependency Setup
+    get_list >> cloud_fmask_processing >> topo_masks_processing >> surface_reflectance_processing >> water_index_processing >> fractional_cover_processing
+
+dag_instance = daily_sentinel_batch_processing_dag()
--- dags/test_dmt_async.py
+++ dags/test_dmt_async.py
@@ -1,13 +1,27 @@
 from datetime import datetime, timedelta
 import time
 from airflow import DAG
+from airflow.utils.task_group import TaskGroup
 from airflow.operators.python import PythonOperator
-from airflow.decorators import dag, task
 
+def get_data_samples():
+    # Simulated list of image identifiers
+    return [5, 10, 12]
 
-# Simulating the delay within each function
-def simulate_delay(seconds):
-    time.sleep(seconds)
+def download_image(sample):
+    print(f"Downloading {sample}")
+    time.sleep(sample)  # Simulate a 5-second download time
+    return f"{sample}_downloaded"
+
+def process_image(sample):
+    print(f"Processing {sample}")
+    time.sleep(sample)  # Simulate a 10-second processing time
+    return f"{sample}_processed"
+
+def upload_image(sample):
+    print(f"Uploading {sample}")
+    time.sleep(sample)  # Simulate a 5-second upload time
+    return f"{sample}_uploaded"
 
 # DAG Configuration
 default_args = {
@@ -20,42 +34,35 @@ default_args = {
     'start_date': datetime(2024, 11, 1),
 }
 
-@dag(
-    dag_id="test_dmt_parallel",
+
+with DAG(
+    dag_id="test_continuous_image_processing",
+    description='Test loop async',    
     default_args=default_args,
-    description='Test dmt async',    
-    schedule=None,
-    catchup=True,
-    #max_active_runs=1,  # <-- I have tried removing this, and the problem persists. 
-)
-def image_processing_dag():
-    @task
-    def get_data_samples():
-        return [5, 10, 12]
+    schedule_interval=None,  # No automatic triggering
+    tags=["dynamic", "image_processing", "parallel"]
+) as dag:
 
-    @task(multiple_outputs=True)
-    def download_image(sample):
-        print(f"Downloading {sample}")
-        simulate_delay(sample)
-        return {"sample": sample, "status": "downloaded"}
+    samples = [5, 10, 12]
+    
+    for sample in samples:
+        with TaskGroup(group_id=f'process_{sample}') as tg:
+            download = PythonOperator(
+                task_id=f'download_{sample}',
+                python_callable=download_image,
+                op_args=[sample]
+            )
 
-    @task(multiple_outputs=True)
-    def process_image(data):
-        sample = data["sample"]
-        print(f"Processing {sample}")
-        simulate_delay(sample)
-        return {"sample": sample, "status": "processed"}
+            process = PythonOperator(
+                task_id=f'process_{sample}',
+                python_callable=process_image,
+                op_args=[sample]
+            )
 
-    @task
-    def upload_image(data):
-        sample = data["sample"]
-        print(f"Uploading {sample}")
-        simulate_delay(sample)
-        return f"{sample} uploaded"
+            upload = PythonOperator(
+                task_id=f'upload_{sample}',
+                python_callable=upload_image,
+                op_args=[sample]
+            )
 
-    samples = get_data_samples()
-    downloaded = download_image.expand(sample=samples)
-    processed = process_image.expand(data=downloaded)
-    uploaded = upload_image.expand(data=processed)
-
-image_processing_flow = image_processing_dag()
+            download >> process >> upload
