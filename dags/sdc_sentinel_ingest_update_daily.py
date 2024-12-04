91b0072305e90adbb703b2ce2cad9140b755a3e2
longle-intersect <long@intersect.org.au>
2024-12-04 14:07:05 +0000

    Update DAG

--- dags/sdc_sentinel_ingest_update_daily.py
+++ dags/sdc_sentinel_ingest_update_daily.py
@@ -81,7 +81,7 @@ def daily_sentinel_batch_ingest_processing_dag():
         provide_context=True
     )
 
-    execution_date = datetime(2024, 12, 5)
+    execution_date = datetime.now(tz=datetime.timezone.utc)
     dates = XCom.get_one(execution_date=execution_date,
                          task_id="get_new_list",
                          dag_id="sdc_sentinel_batch_ingest_update_daily")   
